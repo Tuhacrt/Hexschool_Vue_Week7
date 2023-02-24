d6faca0a01249f2161442366e3a2ae77c6cb8496
Tuhacrt <a067800497@gmail.com>
2023-02-24 11:57:42 +0800

    Update router-link to backstage

--- src/views/user/HomeView.vue
+++ src/views/user/HomeView.vue
@@ -4,7 +4,7 @@
       <router-link to="/"> Home Page </router-link>
       <router-link to="/products"> Products </router-link>
       <router-link to="/cart"> Cart </router-link>
-      <router-link to="/admin"> Dashboard </router-link>
+      <router-link to="/admin/products"> Dashboard </router-link>
     </nav>
     <router-view />
   </div>
