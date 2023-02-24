b9fdd286788c7934d184fb2195c73123f8874436
Tuhacrt <a067800497@gmail.com>
2023-02-25 00:04:16 +0800

    deploy

---
+++ assets/useModal-798cfd3b.js
@@ -0,0 +1,1 @@
+import{l as s,M as a,f as l}from"./index-828dac57.js";const t=(e=l(""))=>{let o=null;return s(()=>{o=new a(e.value,{keyboard:!1,backdrop:!1})}),{modal:o,showModal:()=>o==null?void 0:o.show(),hideModal:()=>o==null?void 0:o.hide()}};export{t as u};
